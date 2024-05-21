3793a139efad9e8e2c02902df286c35f0ea834db
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-05-21 14:47:44 +0200

    Setup webpack

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
